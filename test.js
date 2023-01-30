df483972c4acbe4149d7e4a62488b1b24df7870f
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-01-30 18:09:27 -0500

    loops

---
+++ test.js
@@ -0,0 +1,25 @@
+let numbers = [];
+for (let i = 1; i <= 10; i++) {
+  numbers.push(i);
+}
+console.log(numbers);
+
+let numbers = [];
+let i = 1;
+while (i <= 10) {
+  numbers.push(i);
+  i++;
+}
+console.log(numbers);
+
+let oddNumbers = [];
+for (let i = 11; i <= 21; i += 2) {
+  oddNumbers.push(i);
+}
+console.log(oddNumbers);
+
+let backwardsNumbers = [];
+for (let i = 15; i >= 1; i -= 2) {
+  backwardsNumbers.push(i);
+}
+console.log(backwardsNumbers);
